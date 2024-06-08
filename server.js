ad4604a2f063a5a698764ab9097a576e1b71fe0a
menu95 <100180464+menu95@users.noreply.github.com>
2024-06-07 21:29:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MEGA_VIDEO_URL = 'https://mega.nz/embed/F7VWDKrZ#Z8IWA-ATHF-DGgCiorIgWtxez3JzjFJLOa-U9aDgiIc!9000s1a';
 
-// Middleware para fazer proxy para o Mega
+// Middleware para fazer proxy para o Mega com MIME type de vídeo
 app.use('/video', createProxyMiddleware({
     target: MEGA_VIDEO_URL,
     changeOrigin: true,
     pathRewrite: {
         '^/video': '', // Remove o prefixo /video
+    },
+    onProxyRes(proxyRes, req, res) {
+        proxyRes.headers['content-type'] = 'video/mp4';
     },
     onProxyReq(proxyReq, req, res) {
         proxyReq.setHeader('Referer', 'https://mega.nz/');
